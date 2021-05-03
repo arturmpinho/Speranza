2d746167af93ab0bc9d81a6d7440a8cea8c3b54b
arturmpinho <arturpinho@gmail.com>
2021-05-03 09:44:33 +0000

    Add comments to code in app.py and additional comments to clinical_trials.html, comments.html and mytrials.html

--- app.py
+++ app.py
@@ -34,16 +34,24 @@ def home():
     Function to load the landing page and get 5 last comments
     """
     comments = mongo.db.comments.find().limit(5).sort('posted_on', -1)
+
+    # Convert comments into list for loop within loop functionality
     last5comments = []
     for comment in comments:
         last5comments.append(comment)
 
+    # For each comment, find the corresponding trial when found
+    # If trial not found (due to being removed from API),
+    # flash appropriate message to user
     all_trials = []
     for comment in last5comments:
         result = client.trials.searchTrials(
             q=comment["trial_id"]).result()
         if result["total_count"] != 0:
             all_trials.append(result)
+        else:
+            flash(f'Trial corresponding with comment {comment.user_comment} \
+                not found.')
 
     return render_template('pages/home.html',
                            last5comments=last5comments, all_trials=all_trials)
@@ -91,13 +99,13 @@ def login():
             # check password hash matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                        session['user'] = str(mongo.db.users.find_one(
-                            {"username": request.form.get(
-                                'username').lower()})['_id'])
-                        flash("Welcome, {}".format(
-                            existing_user["fname"].capitalize()))
-                        return redirect(url_for(
-                            'home', user_id=session['user']))
+                session['user'] = str(mongo.db.users.find_one(
+                    {"username": request.form.get(
+                        'username').lower()})['_id'])
+                flash("Welcome, {}".format(
+                    existing_user["fname"].capitalize()))
+                return redirect(url_for(
+                    'home', user_id=session['user']))
 
             else:
                 # invalid password match
@@ -112,18 +120,27 @@ def login():
 
 @app.route('/clinical_trials', methods=["GET", "POST"])
 def clinical_trials():
+    """
+    Function to load clinical trials from API
+    """
 
+    # Load all comments and convert to list to enable loop
+    # within loop on add trial modal
     mongo_comments = mongo.db.comments.find().sort('posted_on', -1)
     comments = []
     for comment in mongo_comments:
         comments.append(comment)
-    if request.method == "POST":
 
+    # Search functionality that retrieves coresponding trials from API
+    # Limited to max 100 trials due to restriction from API
+    if request.method == "POST":
         search = "cancer&immunotherapy&"+request.form.get('search')
 
         trials = client.trials.searchTrials(
             q=search, per_page=100).result()
 
+        # If more than 100 trials are found, set count to 100
+        # As maximum 100 trials will be retrieved / displayed
         count = trials['total_count']
         if count > 100:
             count = 100
@@ -150,9 +167,11 @@ def clinical_trials():
                                trials=trials, count=count)
 
     else:
+        # On loading of page, show the first 100 trials available
         trials = client.trials.searchTrials(
             q='immunotherapy&cancer', per_page=100).result()
 
+        # Show accurate counter to users know the total available trials
         count = trials['total_count']
 
         my_trials = mongo.db.trials.find({'user_id': session.get('user')})
@@ -166,6 +185,9 @@ def clinical_trials():
 
 @app.route('/add_trial/', methods=['GET', 'POST'])
 def add_trial():
+    """
+    Function to add trial to your favorites
+    """
     if request.method == "POST":
         favourite = {
                 'id': request.form.get('trial_api_id'),
@@ -183,9 +205,12 @@ def add_trial():
 
 @app.route('/my_trials/<user_id>', methods=['GET', 'POST'])
 def my_trials(user_id):
-
+    """
+    Function to display the trials added to your favorites
+    and display the comments written by that specific user
+    """
     if session['user']:
-
+        # Retrieve all trials added to favourites for specific user
         trials_mdb = mongo.db.trials.find({'user_id': user_id})
         trials = []
 
@@ -195,13 +220,13 @@ def my_trials(user_id):
             if trial_api["total_count"] != 0:
                 trials.append(trial_api)
 
-        # My reviews
-
+        # Retrieve all the comments and convert to list
         mongo_comments = mongo.db.comments.find().sort('posted_on', -1)
         comments = []
         for comment in mongo_comments:
             comments.append(comment)
 
+        # For each comment, find corresponding trial
         all_trials = []
         for comment in comments:
             result = client.trials.searchTrials(
@@ -209,6 +234,10 @@ def my_trials(user_id):
             if result["total_count"] != 0:
                 if result not in all_trials:
                     all_trials.append(result)
+            # if trial not found, flash appropriate message
+            else:
+                flash(f'Trial corresponding with comment {comment.user_comment} \
+                not found.')
 
         return render_template(
             'pages/mytrials.html', user_id=user_id,
@@ -220,17 +249,22 @@ def my_trials(user_id):
 
 @app.route('/view_trial/<trial_id>', methods=['GET', 'POST'])
 def view_trial(trial_id):
+    """
+    Function to display specific trial when clicking name of trial in comment
+    Redirects to clinical trials page where trial id is inserted in search
+    """
+    # Retrieve all comments and convert to list
     mongo_comments = mongo.db.comments.find().sort('posted_on', -1)
     comments = []
     for comment in mongo_comments:
         comments.append(comment)
 
+    # Search specific trial based on trial id
     search = "cancer&immunotherapy&"+trial_id
     trials = client.trials.searchTrials(
         q=search).result()
     if trials["total_count"] != 0:
         count = trials['total_count']
-
         if not session.get('user'):
             return render_template('pages/clinical_trials.html',
                                    search=search, count=count, trials=trials)
@@ -245,12 +279,14 @@ def view_trial(trial_id):
                                    count=count, public=True)
     else:
         flash('Trial not found.')
-        return redirect(url_for('clinical_trials'))
+        return redirect(url_for('home'))
 
 
 @app.route('/remove_trial/<trial_id>')
 def remove_trial(trial_id):
-
+    """
+    Function to remove trial from your favorites
+    """
     trial_to_remove = mongo.db.trials.find_one({
         'id': trial_id,
         'user_id': session['user']
@@ -264,6 +300,9 @@ def remove_trial(trial_id):
 
 @app.route('/add_comment/<user_id>/<trial_id>', methods=['GET', 'POST'])
 def add_comment(user_id, trial_id):
+    """
+    Function to add a comment to a specific trial
+    """
     if request.method == 'POST':
         user_comment = request.form.get('user_comment')
         user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
@@ -281,7 +320,9 @@ def add_comment(user_id, trial_id):
 
 @app.route('/edit_reviews/<comment_id>', methods=['GET', 'POST'])
 def edit_reviews(comment_id):
-
+    """
+    Function to edit comments/reviews and redirects back to clinical trials
+    """
     comment_to_edit = mongo.db.comments.find_one({
         "_id": ObjectId(comment_id)})
 
@@ -307,7 +348,9 @@ def edit_reviews(comment_id):
 
 @app.route('/delete_reviews/<comment_id>', methods=['GET', 'POST'])
 def delete_reviews(comment_id):
-
+    """
+    Function to delete comments / reviews
+    """
     mongo.db.comments.delete_one({"_id": ObjectId(comment_id)})
 
     return redirect(url_for('clinical_trials'))
